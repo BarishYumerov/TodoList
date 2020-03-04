52b45c5e98837efaf4565ef37447bdbd33442b70
Barish Yumerov <barash@dsi.bg>
2020-03-04 20:25:32 +0200

    fix view when task deadline is in the past and disabled

--- resources/views/todo-list/manage-todo-list-task.blade.php
+++ resources/views/todo-list/manage-todo-list-task.blade.php
@@ -1,21 +1,33 @@
 <tr id="task-row-{{$task->id}}">
     <td>{{{$taskIndex + 1}}}</td>
     <td>
-        <input type="hidden" name="Tasks[{{{$task->id}}}][id]" value="{{{$task->id}}}">
-        <input type="text" name="Tasks[{{{$task->id}}}][name]" value="{{{$task->name}}}">
+        <input type="hidden" name="Tasks[{{{$task->id}}}][id]"
+               @if(strtotime($task->deadline) < $currentTime) disabled @endif
+               value="{{{$task->id}}}">
+        <input type="text" name="Tasks[{{{$task->id}}}][name]"
+               @if(strtotime($task->deadline) < $currentTime) disabled @endif
+               value="{{{$task->name}}}">
         @error('Tasks.' . $task->id . '.name')
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     </td>
     <td>
         <input type="datetime-local" name="Tasks[{{{$task->id}}}][deadline]"
+               @if(strtotime($task->deadline) < $currentTime) disabled @endif
                value="{{date('Y-m-d\TH:i', strtotime($task->deadline))}}">
         @error('Tasks.' . $task->id . '.deadline')
         <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     </td>
-    <td class="small-td center"><input type="checkbox" name="Tasks[{{{$task->id}}}][completed]"
-                                       @if($task->completed == 1) checked @endif></td>
-    <td class="small-td center"><input type="checkbox" name="Tasks[{{{$task->id}}}][disabled]"
-                                       @if($task->disabled == 1) checked @endif></td>
+    <td class="small-td center">
+        <input type="checkbox" name="Tasks[{{{$task->id}}}][completed]"
+               @if($task->disabled == 1) disabled @endif
+               @if(strtotime($task->deadline) < $currentTime) disabled @endif
+               @if($task->completed == 1) checked @endif>
+    </td>
+    <td class="small-td center">
+        <input type="checkbox" name="Tasks[{{{$task->id}}}][disabled]"
+               @if(strtotime($task->deadline) < $currentTime) disabled @endif
+               @if($task->disabled == 1) checked @endif>
+    </td>
 </tr>
--- resources/views/todo-list/manage-todo-list.blade.php
+++ resources/views/todo-list/manage-todo-list.blade.php
@@ -3,6 +3,7 @@
 <link href="{{ asset('css/todo-list/manage-todo-list.css') }}" rel="stylesheet">
 @section('main')
     <div class="manage-todo-list-main">
+        {{$currentTime = strtotime(date('Y-m-d H:i'))}}}
         @include('todo-list.edit-todo-list')
         @include('todo-list.add-new-task-form')
     </div>
